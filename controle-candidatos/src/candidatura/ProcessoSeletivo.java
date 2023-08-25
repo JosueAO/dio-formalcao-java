164666fa51b1cb97efa48dda051e367bb4eba2b8
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2023-08-25 11:11:08 -0300

    Validacaoo de Processo Seletivo - Imprimindo lista candidato selecionado

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -3,7 +3,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		selecaoCandidados();
+		//selecaoCandidados();
+		imprimirSelecionados();
+	}
+	
+	static void imprimirSelecionados() {
+		String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGOSTO"};
+		
+		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+		
+		for(int indice=0; indice < candidatos.length; indice++) {
+			System.out.println("O canditato de nº " + (indice+1) + " é o " + candidatos[indice]);
+		}
+		
+		System.out.println("Forma abreviada de interação for each");
+		
+		for(String candidato: candidatos) {
+			System.out.println("O candidato selecionado foi " + candidato);
+		}
 	}
 
 	static void selecaoCandidados() {	
