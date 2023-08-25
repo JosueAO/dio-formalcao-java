6e429202f4084c2ec32c9debb849a46915ee5548
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2023-08-25 11:38:31 -0300

    Validacaoo de Processo Seletivo - Fazendo ligacoes cada candidato selecionado

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,41 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		//selecaoCandidados();
-		imprimirSelecionados();
+		//imprimirSelecionados();
+		
+		String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGOSTO"};
+		for(String candidato : candidatos) {
+			entrandoEmContato(candidato);
+		}
+		
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+		}while(continuarTentando && tentativasRealizadas < 3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVAS");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + ", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+	}
+	
+	//método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	static void imprimirSelecionados() {
